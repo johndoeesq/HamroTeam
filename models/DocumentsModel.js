11dac8a784894c4b8f90a1f3e4a2cacc3b08e38a
Sanu33 <56817684+johndoeesq@users.noreply.github.com>
2021-12-13 12:22:30 +0545

    ..

--- models/DocumentsModel.js
+++ models/DocumentsModel.js
@@ -1,56 +1,38 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const DocumentsSchema = new mongoose.Schema(
-  {
-
-    files:
-    {
-    type:[String],
-    required: [true, "This field is required"],
-    },
-
-    images:
-    {
-    type:[String],
-    required: [true, "This field is required"],
-    }
-    // CV: {
-    //   type: String,
-    // //   required: [true, "This field is required"],
-    // },
-    // citizenship: {
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-    // passport: {
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-    // PAN: {
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-    // photo :{
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-    // bank_acc: {
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-    // offer_letter:{
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-    // contract: {
-    //     type: String,
-    //     // required: [true, "This field is required"],
-    // },
-  },
-  { timestamps: true }
+	{
+		CV: {
+			type: String,
+			required: [true, 'This field is required'],
+		},
+		citizenship: {
+			type: String,
+			required: [true, 'This field is required'],
+		},
+		passport: {
+			type: String,
+		},
+		PAN: {
+			type: String,
+		},
+		photo: {
+			type: String,
+			required: [true, 'This field is required'],
+		},
+		bank_acc: {
+			type: String,
+		},
+		offer_letter: {
+			type: String,
+		},
+		contract: {
+			type: String,
+		},
+	},
+	{ timestamps: true },
 );
 
-
-const Document = mongoose.model("Document", DocumentsSchema);
+const Document = mongoose.model('Document', DocumentsSchema);
 
 module.exports = Document;
