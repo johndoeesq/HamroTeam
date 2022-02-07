a7e33a74c22995a738ff62aeb218cf0fa5fbd15c
Nirajan Khatri Chhetri <niraxankc@gmail.com>
2022-02-07 11:18:07 +0545

    ..

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,56 +1,59 @@
-const express = require('express');
-const ticketController = require('../controllers/ticketController');
-const allqueryresults = require('../middleware/allqueryresults');
-const Ticket = require('../models/TicketModel');
-const authController = require('../controllers/authController');
+const express = require("express");
+const ticketController = require("../controllers/ticketController");
+const allqueryresults = require("../middleware/allqueryresults");
+const Ticket = require("../models/TicketModel");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
 router
-	.route('/')
-	.get(
-		allqueryresults(Ticket),
-		authController.protect,
-		authController.restrictTo('admin'),
-		ticketController.getAllTicket,
-	)
-	.post(
-		authController.protect,
-		authController.restrictTo('employee'),
-		ticketController.createTicket,
-	);
+  .route("/")
+  .get(
+    allqueryresults(Ticket, {
+      path: "employee",
+      select: "employee_name",
+    }),
+    authController.protect,
+    authController.restrictTo("admin"),
+    ticketController.getAllTicket
+  )
+  .post(
+    authController.protect,
+    authController.restrictTo("employee"),
+    ticketController.createTicket
+  );
 
 router
-	.route('/:id')
-	.get(
-		authController.protect,
-		authController.restrictTo('admin', 'employee'),
-		ticketController.getTicket,
-	)
-	.put(
-		authController.protect,
-		authController.restrictTo('employee'),
-		ticketController.updateTicket,
-	)
-	.delete(
-		authController.protect,
-		authController.restrictTo('admin', 'employee'),
-		ticketController.deleteTicket,
-	);
+  .route("/:id")
+  .get(
+    authController.protect,
+    authController.restrictTo("admin", "employee"),
+    ticketController.getTicket
+  )
+  .put(
+    authController.protect,
+    authController.restrictTo("employee"),
+    ticketController.updateTicket
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo("admin", "employee"),
+    ticketController.deleteTicket
+  );
 router
-	.route('/handle/status/:id')
-	.put(
-		authController.protect,
-		authController.restrictTo('admin'),
-		ticketController.handleTicket,
-	);
+  .route("/handle/status/:id")
+  .put(
+    authController.protect,
+    authController.restrictTo("admin"),
+    ticketController.handleTicket
+  );
 
 router
-	.route('/dismiss/status/:id')
-	.put(
-		authController.protect,
-		authController.restrictTo('admin'),
-		ticketController.dismissTicket,
-	);
+  .route("/dismiss/status/:id")
+  .put(
+    authController.protect,
+    authController.restrictTo("admin"),
+    ticketController.dismissTicket
+  );
 
 module.exports = router;
